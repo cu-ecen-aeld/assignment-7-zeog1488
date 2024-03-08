e7061339e2ea29eff44b1d2044de367f41c6f0c5
Zehariah Oginsky <zeog1488@colorado.edu>
2024-03-08 14:46:20 -0700

    Added username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -1,13 +1,13 @@
-/*                                                     
- * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $ 
- */                                                    
+/*
+ * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "zeog1488\n");
 	return 0;
 }
 
